1065c3029b6571a3a73ff3b04bcb1dcee60f769a
Bobby Neal <rsneal01@gmail.com>
2020-05-11 17:32:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   
   # holiday_hash.each do |season, day|
     
-    holiday_hash[:season][:(holiday_name)] = supply_array
+    holiday_hash[:season][:holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
