ac87afbca545a79ec539f3da1d2c37a802ba2b29
Bobby Neal <rsneal01@gmail.com>
2020-05-11 16:40:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   
   holiday_hash.each do |season, day|
     if season == :winter
-      day << supply
+      day.each do | << supply
     end
   end
 end
