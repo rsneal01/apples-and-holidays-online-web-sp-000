cc09a5126d02688ef214c1340b63254df76b8f9d
Bobby Neal <rsneal01@gmail.com>
2020-05-11 17:33:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # holiday_hash.each do |season, day|
     
     holiday_hash[:season][:holiday_name] = supply_array
+    holiday_hash
 end
-holiday_hash
+
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
