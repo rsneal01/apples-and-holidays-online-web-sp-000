390e013b045dcf8b67b0df31407062f475312b32
Bobby Neal <rsneal01@gmail.com>
2020-05-11 17:06:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   
-  holiday_hash[:spring][:mothers_day] = 
+  holiday_hash[:spring][:mothers_day] = "flowers"
 
 end
 
