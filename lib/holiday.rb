b77dfb4b4b7ff0fdb7ae85584d56a99f06c32974
Bobby Neal <rsneal01@gmail.com>
2020-05-11 18:20:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  holiday_hash.each do |season, day|
+    if season == :winter 
+      
 
 end
 
